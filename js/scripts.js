2e96fc1b32f9d25a403dac491f1f5ae1f2ceb07a
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-17 11:55:30 -0800

    fix bug in double button in first radio.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
   $("input[type='button']").click(function () {
     var radioValue = $("input[name='language']:checked").val();
     var otherValue = $("input[name='starwars']:checked").val();
-    if (radioValue === 'Python' && otherValue === 'darthvader') {
+    if (radioValue === 'Python' || otherValue === 'darthvader') {
       $("#listpython").show();
       $("#listrust").hide();
       $("#listswift").hide();
-    } else if (radioValue === 'Rust' && otherValue != 'chewy' ) {
+    } else if (radioValue === 'Rust' || otherValue != 'chewy' ) {
       $("#listrust").show();
       $("#listpython").hide();
       $("#listswift").hide();
@@ -15,6 +15,9 @@ $(document).ready(function () {
       $("#listrust").hide();
       $("#listpython").hide();
     }
+
+    event.preventDefault();
+    document.getElementById(value).reset();
   });
 });
 
