01c5341c4975c653bee3cf800c51acc8c8b536ab
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-17 13:54:57 -0800

    put results in panel, try to debug reseet remote.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
   $("input[type='button']").click(function () {
+    
     var radioValue = $("input[name='language']:checked").val();
     var otherValue = $("input[name='starwars']:checked").val();
     if (radioValue === 'Python' || otherValue === 'darthvader') {
       $("#listpython").show();
       $("#listrust").hide();
       $("#listswift").hide();
-    } else if (radioValue === 'Rust' || otherValue != 'chewy' ) {
+    } else if (radioValue === 'Rust' || otherValue != 'chewy') {
       $("#listrust").show();
       $("#listpython").hide();
       $("#listswift").hide();
@@ -18,7 +19,11 @@ $(document).ready(function () {
 
     event.preventDefault();
   });
+
+  
 });
+
+
 
 
 // var favoriteColor = $("#color").val(); think this is proper form for value for color wheel, test.
